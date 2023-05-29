3fd4b396eeca90670c2e27fc197a9b1e59855c14
TALEX1995 <tanase.alex@libero.it>
2023-05-29 20:10:10 +0200

    Added validation for userNameInput

--- script/script.js
+++ script/script.js
@@ -22,87 +22,89 @@ const seniorDiscount = 40 / 100;
 // Add event on click button + validation
 
 generateTicket.addEventListener('click', function(){
-    if (isNaN(userKmInput.value) || isNaN(userAgeInput.value)) {
+    if ((!userNameInput.value) || (!isNaN(userNameInput.value))) {
+        alert ('E necessario inserire un nome')
+    } else if (isNaN(userKmInput.value) || isNaN(userAgeInput.value)) {
         alert ('E necessario inserire un numero');
     } else if (userKmInput.value < 1 || userAgeInput.value < 1) {
         alert ('E necessario inserire un numero maggiore di 1');
     } else {
-    // Recovery input value
-    const userName = userNameInput.value.trim();
-    const userKm = parseInt(userKmInput.value);
-    const userAge = parseInt(userAgeInput.value);
-    console.log(userName, userKm, userAge);
+        // Recovery input value
+        const userName = userNameInput.value.trim();
+        const userKm = parseInt(userKmInput.value);
+        const userAge = parseInt(userAgeInput.value);
+        console.log(userName, userKm, userAge);
 
-    // Recovery element from DOM
-    const userNameTicket = document.getElementById('user-name-ticket');
-    const typeTicket = document.getElementById('type-ticket');
-    const priceTicketPlaceholder = document.getElementById('price-ticket');
-    const userNoticeDiscountPlaceholder = document.getElementById('user-notice-discount');
-    const carriage = document.getElementById('carriage');
-    const cpCode = document.getElementById('cp-code');
+        // Recovery element from DOM
+        const userNameTicket = document.getElementById('user-name-ticket');
+        const typeTicket = document.getElementById('type-ticket');
+        const priceTicketPlaceholder = document.getElementById('price-ticket');
+        const userNoticeDiscountPlaceholder = document.getElementById('user-notice-discount');
+        const carriage = document.getElementById('carriage');
+        const cpCode = document.getElementById('cp-code');
 
-    // Calculate ticket price
-    const priceTicket = userKm * kmPrice;
-    console.log(priceTicket);
+        // Calculate ticket price
+        const priceTicket = userKm * kmPrice;
+        console.log(priceTicket);
 
-    // Calculate ticket price discount and ticket type
+        // Calculate ticket price discount and ticket type
 
-    let finalTicketPrice = priceTicket
+        let finalTicketPrice = priceTicket
 
-    let typeTicketCalc = 'Biglietto Standard*'
+        let typeTicketCalc = 'Biglietto Standard*'
 
-    let userNoticeDiscount = '*Considerata la sua età non rientra in alcuna fascia per poter usufruire dello sconto sul biglietto.';
+        let userNoticeDiscount = '*Considerata la sua età non rientra in alcuna fascia per poter usufruire dello sconto sul biglietto.';
 
-    if (userAge < 18) {
-        finalTicketPrice -= finalTicketPrice * youthDiscount;
-        userNoticeDiscount = '*Considerata la sua fascia d\'età rientra nello sconto giovani del 20%'
-        typeTicketCalc = 'Biglietto Under 18'
-    }   else if (userAge >= 65) {
-        finalTicketPrice -= finalTicketPrice * seniorDiscount;
-        userNoticeDiscount = '*Considerata la sua fascia d\'età rientra nello sconto anziani del 40%';
-        typeTicketCalc = 'Biglietto Over 65'
-    }
+        if (userAge < 18) {
+            finalTicketPrice -= finalTicketPrice * youthDiscount;
+            userNoticeDiscount = '*Considerata la sua fascia d\'età rientra nello sconto giovani del 20%'
+            typeTicketCalc = 'Biglietto Under 18'
+        }   else if (userAge >= 65) {
+            finalTicketPrice -= finalTicketPrice * seniorDiscount;
+            userNoticeDiscount = '*Considerata la sua fascia d\'età rientra nello sconto anziani del 40%';
+            typeTicketCalc = 'Biglietto Over 65'
+        }
 
-    console.log(typeTicketCalc, userNoticeDiscount, finalTicketPrice.toFixed(2));
+        console.log(typeTicketCalc, userNoticeDiscount, finalTicketPrice.toFixed(2));
 
-    // Carriage Random Number
+        // Carriage Random Number
 
-    const randomCarriageNumber = Math.floor((Math.random() * 10) +1);
-    console.log(randomCarriageNumber);
+        const randomCarriageNumber = Math.floor((Math.random() * 10) +1);
+        console.log(randomCarriageNumber);
 
-    // CP Code random number
+        // CP Code random number
 
-    const randomCpCode = Math.floor((Math.random() * (100000 - 10000)) +10000);
-    console.log(randomCpCode);
+        const randomCpCode = Math.floor((Math.random() * (100000 - 10000)) +10000);
+        console.log(randomCpCode);
 
 
-    // Write final price, discount, Name, ticket type, carriage CP and Code into DOM 
+        // Write final price, discount, Name, ticket type, carriage CP and Code into DOM 
 
-    // User info 
-    userNameTicket.innerText = userName
+        // User info 
+        userNameTicket.innerText = userName
 
-    // Type Ticket
-    typeTicket.innerText = typeTicketCalc;
+        // Type Ticket
+        typeTicket.innerText = typeTicketCalc;
 
-    // Price Ticket
-    priceTicketPlaceholder.innerText = finalTicketPrice.toFixed(2) + 'euro';
+        // Price Ticket
+        priceTicketPlaceholder.innerText = finalTicketPrice.toFixed(2) + 'euro';
 
-    // Discount
-    userNoticeDiscountPlaceholder.innerText = userNoticeDiscount;
+        // Discount
+        userNoticeDiscountPlaceholder.innerText = userNoticeDiscount;
 
-    // Carriage random number into DOM
-    carriage.innerText = randomCarriageNumber
+        // Carriage random number into DOM
+        carriage.innerText = randomCarriageNumber
 
-    // CP Code random number indo DOM
-    cpCode.innerText = randomCpCode
+        // CP Code random number indo DOM
+        cpCode.innerText = randomCpCode
 
-    // Ticket Display when click button generate
+        // Ticket Display when click button generate
 
-    const ticket = document.querySelector('.ticket');
-    console.log(ticket);
-    
-    ticket.classList.remove('d-none');
-    }
+        const ticket = document.querySelector('.ticket');
+        console.log(ticket);
+        
+        ticket.classList.remove('d-none');
+        }
 })
  
 
