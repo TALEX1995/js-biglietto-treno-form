3178d56d68f76d256ceefde3123a96c359f8fa3c
TALEX1995 <tanase.alex@libero.it>
2023-05-29 16:47:35 +0200

    Added passenger name and ticket type

--- script/script.js
+++ script/script.js
@@ -31,40 +31,62 @@ generateTicket.addEventListener('click', function(){
     const userAge = userAgeInput.value;
     console.log(userName, userKm, userAge);
 
-
-})
- 
-
+    // Recovery element from DOM
+    const userNameTicket = document.getElementById('user-name-ticket');
+    const typeTicket = document.getElementById('type-ticket');
 
     // Calculate ticket price
-
     const priceTicket = userKm * kmPrice;
     console.log(priceTicket);
 
-
-    // Calculate ticket price and discount
+    // Calculate ticket price discount and ticket type
 
     let finalTicketPrice = priceTicket
+
+    let typeTicketCalc = 'Biglietto Standard'
 
     let userNoticeDiscount = 'Considerata la sua età non rientra in alcuna fascia per poter usufruire dello sconto sul biglietto.';
 
     if (userAge < 18) {
         finalTicketPrice -= finalTicketPrice * youthDiscount;
         userNoticeDiscount = 'Considerata la sua fascia d\'età rientra nello sconto giovani del 20%'
-}   else if (userAge >= 65) {
+        typeTicketCalc = 'Biglietto Under 18'
+    }   else if (userAge >= 65) {
         finalTicketPrice -= finalTicketPrice * seniorDiscount;
         userNoticeDiscount = 'Considerata la sua fascia d\'età rientra nello sconto anziani del 40%';
-}
+        typeTicketCalc = 'Biglietto Over 65'
+    }
+
+    console.log(typeTicketCalc);
 
     console.log(userNoticeDiscount);
 
     console.log(finalTicketPrice.toFixed(2));
 
-    // Write final price and discount into DOM 
+    // Write final price, discount, Name, ticket type, carriage CP and Code into DOM 
 
+    // User info 
+    userNameTicket.innerText = userName
+
+    // Type Ticket
+    typeTicket.innerText = typeTicketCalc;
+
+    // Price Ticket
     priceTicketPlaceholder.innerText = finalTicketPrice.toFixed(2);
 
+    // Discount
     userNoticeDiscountPlaceholder.innerText = userNoticeDiscount;
+
+})
+ 
+
+
+    
+
+
+    
+
+    
 
 
     // Write Discount in the DOM
